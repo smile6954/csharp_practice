ced0a594fd59e6e9ca5d34d24e84888d7aa9f06c
Leonid Soloviev <solovievleon@gmail.com>
2022-04-19 20:58:21 +0300

    определение корня из двух чисел

--- lesson1/exmpl1/Program.cs
+++ lesson1/exmpl1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Vvedite pervoe chislo");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Vvedite vtoroe chislo");
+int num2 = int.Parse(Console.ReadLine());
+int a = num1*num1;
+if(num2==a)
+    {Console.WriteLine("Koren");
+    }
+else
+    {Console.WriteLine("ne koren");
+    }
