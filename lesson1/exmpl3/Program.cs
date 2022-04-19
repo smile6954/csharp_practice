38884be882f1bb41d3eb2e9e7d3699af26899b5f
Leonid Soloviev <solovievleon@gmail.com>
2022-04-19 21:28:34 +0300

    Нахождение остатка от деления. Нашли последнюю и первую цифру от введенного числа

---
+++ lesson1/exmpl3/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+int num = int.Parse(Console.ReadLine());
+int y = num%10;
+int x = num/1000;
+Console.WriteLine($"Вы ввели число {num}");
+Console.WriteLine($"остаток от деления = {y} \nпервая цифра = {x}");
+
