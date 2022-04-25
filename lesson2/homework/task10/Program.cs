d45a4a86464350028a27542fd057d3adcd6873b9
Leonid Soloviev <solovievleon@gmail.com>
2022-04-25 19:34:06 +0300

    Домашнее задание для 2 урока

---
+++ lesson2/homework/task10/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число");
+String a1 = ReadLine();
+WriteLine(a1[1]);
---
+++ lesson2/homework/task13/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число");
+String a1 = ReadLine();
+if (a1.Length < 3)
+{
+WriteLine("Третьего числа нет");
+}
+else 
+
+WriteLine($"Третье число - {a1[2]}");
